b9d25d7cd5b5cdbad8e79854e556d6be9a119acb
sissh <sissani.hani@gmail.com>
2021-04-16 20:57:30 +0200

    javadoc

--- src/Parser/Fonctions.java
+++ src/Parser/Fonctions.java
@@ -12,34 +12,38 @@ import Vue.FenetreMere;
 
 public class Fonctions {
 
-/**Cette méthode reçoit le nom de la fonction demandées et ses arguments: elle vérifie la validité des arguments et exécute la fonction demandée**/	
+/**Cette méthode reçoit le nom de la fonction demandées et ses arguments: elle vérifie la validité des arguments et exécute la fonction demandée	
+ * @param nomFonction
+ * @param argsArrayList
+ * @return
+ */
 	public static Object execFonction(String nomFonction, ArrayList<Object> argsArrayList) { 
 		Object [] arguments = argsArrayList.toArray();
 		
-		switch(nomFonction) {			/**Switch qui filtre les erreurs d'arguments et exécute les fonctions demandées: renvoi un msg d'erreur si arg invalide**/
+		switch(nomFonction) {			//Switch qui filtre les erreurs d'arguments et exécute les fonctions demandées: renvoi un msg d'erreur si arg invalide
 			case "pow":
-				if (arguments.length < 2) {	/**Pas assez d'arguments**/
+				if (arguments.length < 2) {	//Pas assez d'arguments
 					return messageErreur(3);
 				}
 				
-				if (arguments.length > 2) {							/**Trop d'arguments**/
+				if (arguments.length > 2) {							//Trop d'arguments
 					return messageErreur(4);
 				}
 				
-				if (!(arguments[0] instanceof Integer) && !(arguments[1] instanceof Integer)) {		/**Aucun argument valide**/
+				if (!(arguments[0] instanceof Integer) && !(arguments[1] instanceof Integer)) {		//Aucun argument valide
 					return messageErreur(5);
 				}
 				
-				else if (arguments[0] instanceof Integer && !(arguments[1] instanceof Integer) ) {		/**Premier argument valide, second non**/
+				else if (arguments[0] instanceof Integer && !(arguments[1] instanceof Integer) ) {		//Premier argument valide, second non
 					return messageErreur(1);
 				}
 				
-				else if (!(arguments[0]instanceof Integer) && arguments[1] instanceof Integer) {		/**Second argument valide, premier non**/
+				else if (!(arguments[0]instanceof Integer) && arguments[1] instanceof Integer) {		//Second argument valide, premier non
 					return messageErreur(2);
 				}
 				
 				else {
-					return pow(arguments);			/**Exécution de pow**/
+					return pow(arguments);			//Exécution de pow
 				}
 			
 			case "sqrt":
@@ -68,12 +72,15 @@ public class Fonctions {
 					return messageErreur(6);
 				}
 				
-			default: return messageErreur(42);			/**Fonction non reconnue : ne devrait pas apparaître car non appelé par le parser**/
+			default: return messageErreur(42);			//Fonction non reconnue : ne devrait pas apparaître car non appelé par le parser
 		}
 
 	}
 
-/**Cette méthode privée répertorie les différents message d'erreur selon leur code d'erreur, pour la lisibilité.**/
+/**Cette méthode privée répertorie les différents message d'erreur selon leur code d'erreur, pour la lisibilité.
+ * @param codeErreur
+ * @return
+ */
 	private static String messageErreur(int codeErreur) {
 		switch(codeErreur) {
 			case 0: return "Aucun problï¿½me rencontrï¿½.";
@@ -91,18 +98,26 @@ public class Fonctions {
 		}
 	}
 
-/**Fonction puissance du C **/	
+/**Fonction puissance du C 
+ * 	
+ * @param arguments
+ * @return
+ */
 	public static int pow(Object [] arguments) {	
-		int nb = Integer.parseInt(arguments[0].toString());				/**Récupération des type Object du parser et conversion en Int**/
+		int nb = Integer.parseInt(arguments[0].toString());				//Récupération des type Object du parser et conversion en Int
 		int puiss = Integer.parseInt(arguments[1].toString());
 		int resultat = 1;
 		for (int i = 0; i < puiss; i++) {
-			resultat = resultat * nb;				/**Calcul de puissance**/
+			resultat = resultat * nb;				//Calcul de puissance
 		}		
 		return resultat;		
 	}
 	
-/**Fonction racine carré du C**/
+/**Fonction racine carré du C
+ * 
+ * @param arguments
+ * @return
+ */
 	public static int sqrt(Object [] arguments) {
 		double nb = Double.parseDouble(arguments[0].toString());
 		double i;
@@ -115,7 +130,11 @@ public class Fonctions {
 		return resultatInt;
 	}
 	
-/**Fonction exponentielle du C**/
+/**Fonction exponentielle du C
+ * 
+ * @param arguments
+ * @return
+ */
 	public static int exp(Object [] arguments) {
 		double resultat = Math.exp(Double.parseDouble(arguments[0].toString()));
 		int resultatInt = (int)resultat;
@@ -123,26 +142,27 @@ public class Fonctions {
 	}
 	
 	
-/**Fonction printf du C	**/
+/**Fonction printf du C	
+ * @param arguments
+ * @return
+ */
 	public static int print(Object [] arguments) {			
-		String phrase = arguments[0].toString();								/**La fonction travail avec des String, pour la méthode subString()**/
+		String phrase = arguments[0].toString();								//La fonction travail avec des String, pour la méthode subString()
 		int argCourant = 1;
 		int i = 0;
-		int nbArgs = arguments.length - 1;										/**Compte le nb d'arguments**/
-		int nbTrigger = nbArgs;														/**Compte le nb de % détectés par la fonction: on part du principe qu'il y en aura autant que d'args**/
+		int nbArgs = arguments.length - 1;										//Compte le nb d'arguments
+		int nbTrigger = nbArgs;														//Compte le nb de % détectés par la fonction: on part du principe qu'il y en aura autant que d'args
 		
-		while (i < phrase.length()-1 && nbArgs > 0) {			/**Parcours de la phrase: taille variable car elle est changée quand on trouve des %; pas de parcours si pas d'arguments.**/
+		while (i < phrase.length()-1 && nbArgs > 0) {			//Parcours de la phrase: taille variable car elle est changée quand on trouve des %; pas de parcours si pas d'arguments.
 			
-			if (phrase.charAt(i) == '%') {		/**Détection d'un %**/
+			if (phrase.charAt(i) == '%') {		//Détection d'un %
 				
-				if (i < phrase.length() - 1 && testVariable(phrase.charAt(i+1), arguments[argCourant])==0) {		/**Vérifie si le charactère qui suit % fait parti de ceux appelant un argument**/
+				if (i < phrase.length() - 1 && testVariable(phrase.charAt(i+1), arguments[argCourant])==0) {		//Vérifie si le charactère qui suit % fait parti de ceux appelant un argument
 					if (nbTrigger > 0) {
-						phrase = phrase.substring(0, i) + arguments[argCourant].toString() + phrase.substring(i+2);	/**Découpe du String : **/
-						argCourant += 1;									/** On coupe jusqu'à avant le %, on concatène l'argument puis le reste de la phrase**/
+						phrase = phrase.substring(0, i) + arguments[argCourant].toString() + phrase.substring(i+2);	//Découpe du String : 
+						argCourant += 1;									// On coupe jusqu'à avant le %, on concatène l'argument puis le reste de la phrase
 					}
-					nbTrigger--;	/**Passe à la variable suivante; ajoute 1 au décompte de % trouvé.**/ 		
-					System.out.println("Nbtrigger:");
-					System.out.println(nbTrigger);
+					nbTrigger--;	//Passe à la variable suivante; ajoute 1 au décompte de % trouvé. 		
 				}
 				
 				else if (i < phrase.length() - 1 && testVariable(phrase.charAt(i+1), arguments[argCourant])==1) {
@@ -155,17 +175,17 @@ public class Fonctions {
 			i++;
 		}
 		
-		if (nbTrigger == 0) {							/**Si il y a bien autant d'appel d'arguments que d'arguments, on renvoie le resultat**/
+		if (nbTrigger == 0) {							//Si il y a bien autant d'appel d'arguments que d'arguments, on renvoie le resultat
 			FenetreMere.affichePrintf(phrase, 0);
 			System.out.println("ok");
 		}
 		
-		else if (nbTrigger < 0) {							/**S'il y a plus d'appel d'arguments que d'arguments, on renvoi une erreur**/
+		else if (nbTrigger < 0) {							//S'il y a plus d'appel d'arguments que d'arguments, on renvoi une erreur
 			FenetreMere.affichePrintf(messageErreur(3), 1);
 			System.out.println("pipi caca");
 		}
 		
-		else {													/**S'il y a moins d'appel d'arguments que d'arguments, on renvoi une erreur**/
+		else {													//S'il y a moins d'appel d'arguments que d'arguments, on renvoi une erreur
 			FenetreMere.affichePrintf(messageErreur(4), 1);
 			System.out.println("zebi");
 		}
@@ -186,10 +206,15 @@ public class Fonctions {
 	}
 
 
-/**Méthode privée utilisée par print permettant de savoir si le caractère suivant % est bien un des % appelant et vérifiant le type de variable correspondant**/
+/**Méthode privée utilisée par print permettant de savoir si le caractère suivant % est bien un des % appelant et vérifiant le type de variable correspondant
+ * 
+ * @param ch1
+ * @param arg
+ * @return
+ */
 	private static int testVariable (char ch1, Object arg) { 
 			switch (ch1) {
-				case 'd':													/**On vérifie si au caractère détecté, on a bien un type d'argument correspondant.**/
+				case 'd':													//On vérifie si au caractère détecté, on a bien un type d'argument correspondant.
 					if (arg instanceof Integer) {return 0;}
 					else; return 1;		
 				case 'c':
